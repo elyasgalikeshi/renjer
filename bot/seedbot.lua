6d275d24cde82c6b159800394a5d608e8dc3296e
elyasgalikeshi <shabakhsh14@gmail.com>
2016-01-09 17:45:52 +0330

    Create seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all",
     "leave_ban"
-    },
+    sudo_users = {83098998},--Sudo users
     sudo_users = {110626080,103649648,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {data = 'data/moderation.json'},--Realms Id
