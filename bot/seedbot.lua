1c5e18aea7589aa6e45b0c42d06d88c86c949a0b
elyasgalikeshi <shabakhsh14@gmail.com>
2016-01-14 19:57:29 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -222,7 +222,7 @@ function create_config( )
     "leave_ban"
     sudo_users = {132633553,154184294},--Sudo users
     disabled_channels = {}
-    realm = {89490142},--Realms Id
+    realm = {132633553},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 
