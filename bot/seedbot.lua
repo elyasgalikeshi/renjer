527b22497d41c89e55f8ca96765bfe238906f1ed
elyasgalikeshi <shabakhsh14@gmail.com>
2016-01-14 18:51:32 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all",
     "leave_ban"
-    sudo_users = {132633553,154184294},--Sudo users
+    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id132633553,154184294)},--Sudo users
     sudo_users = {132633553,154184294},--Sudo users
     realm = {89490142},--Realms Id
     moderation = {data = 'data/moderation.json'},
